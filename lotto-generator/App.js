01f23331c2cb4bb31aff6ebd903e36101da606d4
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2020-07-04 15:03:39 +0900

    수정 : lodash 및 styled-components 적용

--- lotto-generator/App.js
+++ lotto-generator/App.js
@@ -1,32 +1,88 @@
 import * as React from 'react';
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import { Button, Dimensions, SafeAreaView, Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import _ from 'lodash';
+import styled from 'styled-components/native';
+
+let numbers = [];
+// 1부터 45의 숫자를 생성
+_.times( 45, n => numbers.push( n + 1 ) );
+// 섞는다
+// numbers = _.shuffle( numbers );
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding-top: ${ Constants.statusBarHeight }px;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  margin-bottom: 24px;
+`;
+
+const Ball = styled.View`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background: ${ props => {
+    if( props.value < 11 ) {
+      return '#e5e251';
+    }
+    else if( props.value < 21 ) {
+      return '#517FE5';
+    }
+    else if( props.value < 31  ) {
+      return '#E54036';
+    }
+    else if( props.value < 41  ) {
+      return '#e5e5e5';
+    }
+    else {
+      return '#43BF74';
+    }
+  } };
+  justify-content: center;
+  align-items: center;
+`;
+
+const Label = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #000000;
+`;
 
 export default function App() {
+  // 상태 변화 state
+  const [ displayNumbers, setNumbers ] = React.useState( _.shuffle( numbers ) ); // Hook
+  // displayNumbers - 상태의 읽기 전용 값
+  // setNumbers - 상태를 변경하기 위한 업데이트 함수
   // React.Fragment 실제 렌더링에 반영되지 않는 추상 컨테이너
   return (
-    <SafeAreaView style={ styles.container }>
-      <View style={ styles.row }>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>1</Text>
-        </View>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>11</Text>
-        </View>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>21</Text>
-        </View>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>31</Text>
-        </View>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>41</Text>
-        </View>
-        <View style={ styles.ball }>
-          <Text style={ styles.text }>45</Text>
-        </View>
-      </View>
-    </SafeAreaView>
+    <Container>
+      <Row>
+        <Ball value={ displayNumbers[ 0 ] }>
+          <Label>{ displayNumbers[ 0 ] }</Label>
+        </Ball>
+        <Ball value={ displayNumbers[ 1 ] }>
+          <Label>{ displayNumbers[ 1 ] }</Label>
+        </Ball>
+        <Ball value={ displayNumbers[ 2 ] }>
+          <Label>{ displayNumbers[ 2 ] }</Label>
+        </Ball>
+        <Ball value={ displayNumbers[ 3 ] }>
+          <Label>{ displayNumbers[ 3 ] }</Label>
+        </Ball>
+        <Ball value={ displayNumbers[ 4 ] }>
+          <Label>{ displayNumbers[ 4 ] }</Label>
+        </Ball>
+        <Ball value={ displayNumbers[ 5 ] }>
+          <Label>{ displayNumbers[ 5 ] }</Label>
+        </Ball>
+      </Row>
+      <Button title="다시 뽑기" onPress={ () => setNumbers( _.shuffle( numbers ) ) }/>
+    </Container>
   );
 }
 
@@ -39,6 +95,7 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
+    marginBottom: 24,
   },
   ball: {
     width: 50,
