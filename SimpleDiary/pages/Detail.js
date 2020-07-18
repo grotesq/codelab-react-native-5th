92cca5eba1ecb220f11528efa24fd1228cedac73
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2020-07-18 09:42:15 +0900

    navigate()에 params 전송

--- SimpleDiary/pages/Detail.js
+++ SimpleDiary/pages/Detail.js
@@ -2,17 +2,28 @@ import React from 'react';
 import Container from '../components/Container';
 import Contents from '../components/Contents';
 import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Text = styled.Text`
     font-size: 20px;
     line-height: 28px;
 `;
 
-function Detail( { navigation } ) {
+function Detail( { navigation, route } ) {
+    navigation.setOptions( { title: route.params.date } );
+    const [text, setText] = React.useState( '' );
+    React.useEffect(()=>{
+       AsyncStorage.getItem( 'list' )
+        .then( data => {
+            const list = JSON.parse( data );
+            const diary = list.find( element => element.date === route.params.date );
+            setText( diary.text );
+        }) 
+    }, []);
     return (
         <Container>
             <Contents>
-                <Text>{`Lorem Ipsum Dolar Sit Amet...`}</Text>
+                <Text>{ text }</Text>
             </Contents>
         </Container>
     )
--- SimpleDiary/pages/List.js
+++ SimpleDiary/pages/List.js
@@ -4,6 +4,7 @@ import Contents from '../components/Contents';
 import Button from '../components/Button';
 import styled from 'styled-components/native';
 import AsyncStorage from '@react-native-community/async-storage';
+import _ from 'lodash';
 
 const ListItem = styled.TouchableOpacity`
     width: 100%;
@@ -35,10 +36,10 @@ function List( { navigation } ) {
     return (
         <Container>
             <Contents>
-                {list.map( item => {
+                { _.sortBy(list, 'date').map( item => {
                     return (
                         <ListItem key={ item.date }
-                            onPress={ () => navigation.navigate( 'Detail' ) }
+                            onPress={ () => navigation.navigate( 'Detail', { date: item.date } ) }
                         >
                             <Label>{item.date}</Label>
                         </ListItem>
